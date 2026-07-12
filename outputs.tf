7b28189dff51f37ddb749d1b72bb664385e4aae3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "cdn_frontdoor_profiles" {
-  description = "All cdn_frontdoor_profile resources"
-  value       = azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles
-}
 output "cdn_frontdoor_profiles_identity" {
-  description = "List of identity values across all cdn_frontdoor_profiles"
-  value       = [for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : v.identity]
+  description = "Map of identity values across all cdn_frontdoor_profiles, keyed the same as var.cdn_frontdoor_profiles"
+  value       = { for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : k => v.identity }
 }
 output "cdn_frontdoor_profiles_log_scrubbing_rule" {
-  description = "List of log_scrubbing_rule values across all cdn_frontdoor_profiles"
-  value       = [for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : v.log_scrubbing_rule]
+  description = "Map of log_scrubbing_rule values across all cdn_frontdoor_profiles, keyed the same as var.cdn_frontdoor_profiles"
+  value       = { for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : k => v.log_scrubbing_rule }
 }
 output "cdn_frontdoor_profiles_name" {
-  description = "List of name values across all cdn_frontdoor_profiles"
-  value       = [for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : v.name]
+  description = "Map of name values across all cdn_frontdoor_profiles, keyed the same as var.cdn_frontdoor_profiles"
+  value       = { for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : k => v.name }
 }
 output "cdn_frontdoor_profiles_resource_group_name" {
-  description = "List of resource_group_name values across all cdn_frontdoor_profiles"
-  value       = [for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : v.resource_group_name]
+  description = "Map of resource_group_name values across all cdn_frontdoor_profiles, keyed the same as var.cdn_frontdoor_profiles"
+  value       = { for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : k => v.resource_group_name }
 }
 output "cdn_frontdoor_profiles_resource_guid" {
-  description = "List of resource_guid values across all cdn_frontdoor_profiles"
-  value       = [for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : v.resource_guid]
+  description = "Map of resource_guid values across all cdn_frontdoor_profiles, keyed the same as var.cdn_frontdoor_profiles"
+  value       = { for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : k => v.resource_guid }
 }
 output "cdn_frontdoor_profiles_response_timeout_seconds" {
-  description = "List of response_timeout_seconds values across all cdn_frontdoor_profiles"
-  value       = [for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : v.response_timeout_seconds]
+  description = "Map of response_timeout_seconds values across all cdn_frontdoor_profiles, keyed the same as var.cdn_frontdoor_profiles"
+  value       = { for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : k => v.response_timeout_seconds }
 }
 output "cdn_frontdoor_profiles_sku_name" {
-  description = "List of sku_name values across all cdn_frontdoor_profiles"
-  value       = [for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : v.sku_name]
+  description = "Map of sku_name values across all cdn_frontdoor_profiles, keyed the same as var.cdn_frontdoor_profiles"
+  value       = { for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : k => v.sku_name }
 }
 output "cdn_frontdoor_profiles_tags" {
-  description = "List of tags values across all cdn_frontdoor_profiles"
-  value       = [for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : v.tags]
+  description = "Map of tags values across all cdn_frontdoor_profiles, keyed the same as var.cdn_frontdoor_profiles"
+  value       = { for k, v in azurerm_cdn_frontdoor_profile.cdn_frontdoor_profiles : k => v.tags }
 }
 
